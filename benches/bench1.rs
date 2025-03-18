c105635a328e90da9f4c8b7584fecafefd5224e5
Pierre-Antoine Champin <pierre-antoine@w3.org>
2025-03-18 09:59:12 +0100

    extend benchmark
    it appears that Cow<str> is actually faster than Mownstr...
    But it is also more space consuming.

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -5,6 +5,8 @@
 //! The results of `borrowed_mownstr` should therefore be compared to `refs`,
 //! and that of `owned_mownstr` should be compared to `strings`.
 
+use std::borrow::Cow;
+
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use mownstr::MownStr;
 
@@ -15,9 +17,8 @@ fn refs(c: &mut Criterion) {
         |b, &i| {
             b.iter(|| {
                 #[allow(clippy::iter_cloned_collect)]
-                // strangely, replacing .iter().cloned().collect() by to_vec(),
-                // as suggested by clippy, causes this test to crash :-/
-                #[allow(clippy::needless_collect)]
+                // clippy suggests using to_vec() instead,
+                // but that makes the comparison unfair with the others.
                 let v = i.iter().copied().collect::<Vec<_>>();
                 assert!(v.len() == i.len());
             });
@@ -31,8 +32,20 @@ fn borrowed_mownstr(c: &mut Criterion) {
         black_box(&STRINGS),
         |b, &i| {
             b.iter(|| {
-                #[allow(clippy::needless_collect)]
                 let v = i.iter().map(|r| MownStr::from(*r)).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn borrowed_cowstr(c: &mut Criterion) {
+    c.bench_with_input(
+        BenchmarkId::new("borrowed_cowstr", ""),
+        black_box(&STRINGS),
+        |b, &i| {
+            b.iter(|| {
+                let v = i.iter().map(|r| Cow::<str>::from(*r)).collect::<Vec<_>>();
                 assert!(v.len() == i.len());
             });
         },
@@ -45,7 +58,6 @@ fn strings(c: &mut Criterion) {
         black_box(&STRINGS),
         |b, &i| {
             b.iter(|| {
-                #[allow(clippy::needless_collect)]
                 let v = i
                     .iter()
                     .map(|r| (*r).to_string())
@@ -63,7 +75,6 @@ fn owned_mownstr(c: &mut Criterion) {
         black_box(&STRINGS),
         |b, &i| {
             b.iter(|| {
-                #[allow(clippy::needless_collect)]
                 let v = i
                     .iter()
                     .map(|r| (*r).to_string())
@@ -75,7 +86,256 @@ fn owned_mownstr(c: &mut Criterion) {
     );
 }
 
-criterion_group!(benches, refs, borrowed_mownstr, strings, owned_mownstr,);
+fn owned_cowstr(c: &mut Criterion) {
+    c.bench_with_input(
+        BenchmarkId::new("owned_cowstr", ""),
+        black_box(&STRINGS),
+        |b, &i| {
+            b.iter(|| {
+                let v = i
+                    .iter()
+                    .map(|r| (*r).to_string())
+                    .map(Cow::<str>::from)
+                    .collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn len_refs(c: &mut Criterion) {
+    let refs = STRINGS.to_vec();
+    c.bench_with_input(
+        BenchmarkId::new("len_refs", ""),
+        black_box(&refs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| j.len()).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn len_borrowed_mownstr(c: &mut Criterion) {
+    let mownstrs = STRINGS
+        .iter()
+        .copied()
+        .map(MownStr::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("len_borrowed_mownstr", ""),
+        black_box(&mownstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| j.len()).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn len_borrowed_cowstr(c: &mut Criterion) {
+    let cowstrs = STRINGS
+        .iter()
+        .copied()
+        .map(Cow::<str>::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("len_borrowed_cowstr", ""),
+        black_box(&cowstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| j.len()).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn len_strings(c: &mut Criterion) {
+    let strings = STRINGS
+        .iter()
+        .copied()
+        .map(String::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("len_strings", ""),
+        black_box(&strings),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| j.len()).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn len_owned_mownstr(c: &mut Criterion) {
+    let mownstrs = STRINGS
+        .iter()
+        .copied()
+        .map(String::from)
+        .map(MownStr::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("len_owned_mownstr", ""),
+        black_box(&mownstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| j.len()).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn len_owned_cowstr(c: &mut Criterion) {
+    let cowstrs = STRINGS
+        .iter()
+        .copied()
+        .map(String::from)
+        .map(Cow::<str>::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("len_owned_cowstr", ""),
+        black_box(&cowstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| j.len()).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn substr_refs(c: &mut Criterion) {
+    let refs = STRINGS.to_vec();
+    c.bench_with_input(
+        BenchmarkId::new("substr_refs", ""),
+        black_box(&refs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| &j[1..3]).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn substr_borrowed_mownstr(c: &mut Criterion) {
+    let mownstrs = STRINGS
+        .iter()
+        .copied()
+        .map(MownStr::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("substr_borrowed_mownstr", ""),
+        black_box(&mownstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| &j[1..3]).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn substr_borrowed_cowstr(c: &mut Criterion) {
+    let cowstrs = STRINGS
+        .iter()
+        .copied()
+        .map(Cow::<str>::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("substr_borrowed_cowstr", ""),
+        black_box(&cowstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| &j[1..3]).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn substr_strings(c: &mut Criterion) {
+    let strings = STRINGS
+        .iter()
+        .copied()
+        .map(String::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("substr_strings", ""),
+        black_box(&strings),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| &j[1..3]).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn substr_owned_mownstr(c: &mut Criterion) {
+    let mownstrs = STRINGS
+        .iter()
+        .copied()
+        .map(String::from)
+        .map(MownStr::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("substr_owned_mownstr", ""),
+        black_box(&mownstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| &j[1..3]).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+fn substr_owned_cowstr(c: &mut Criterion) {
+    let cowstrs = STRINGS
+        .iter()
+        .copied()
+        .map(String::from)
+        .map(Cow::<str>::from)
+        .collect::<Vec<_>>();
+    c.bench_with_input(
+        BenchmarkId::new("substr_owned_cowstr", ""),
+        black_box(&cowstrs),
+        |b, i| {
+            b.iter(|| {
+                let v = i.iter().map(|j| &j[1..3]).collect::<Vec<_>>();
+                assert!(v.len() == i.len());
+            });
+        },
+    );
+}
+
+criterion_group!(
+    benches,
+    refs,
+    borrowed_mownstr,
+    borrowed_cowstr,
+    strings,
+    owned_mownstr,
+    owned_cowstr,
+    len_refs,
+    len_borrowed_mownstr,
+    len_borrowed_cowstr,
+    len_strings,
+    len_owned_mownstr,
+    len_owned_cowstr,
+    substr_refs,
+    substr_borrowed_mownstr,
+    substr_borrowed_cowstr,
+    substr_strings,
+    substr_owned_mownstr,
+    substr_owned_cowstr
+);
 criterion_main!(benches);
 
 const STRINGS: [&str; 30] = [
